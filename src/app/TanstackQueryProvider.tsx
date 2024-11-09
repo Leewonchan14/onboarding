c2353366f3c39a63f3e31f585ef6dad2f492e09e
이원찬 <twoone14@tukorea.ac.kr>
2024-11-09 15:00:51 +0900

    React Query devtool 설정

--- src/app/TanstackQueryProvider.tsx
+++ src/app/TanstackQueryProvider.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 
 const queryClient = new QueryClient();
@@ -7,7 +8,10 @@ const TanstackQueryProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
