87a40064b2b4f49644d7ad1e6fe1e7f0df4a22b8
이원찬 <twoone14@tukorea.ac.kr>
2024-11-10 21:44:40 +0900

    TodoList createButton 분리 및 TodoList, TodoItem 분리

--- src/pages/todo/ui/TodoList.tsx
+++ src/pages/todo/ui/TodoList.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { MainButton } from "../../../shared/ui";
 import { Todo } from "../model/Todo";
+import TodoItem from "./TodoItem";
 
 export function TodoList({
   selectedTodo,
@@ -14,53 +13,15 @@ export function TodoList({
   // TODO TodoList mutation 구현 해야함
 
   return (
-    <div className="w-full h-full flex flex-col py-10 gap-4">
-      <h1 className="text-2xl font-bold flex text-nowrap items-center">
-        할 일 목록
-        <MainButton
-          text="추가"
-          className="ml-auto text-lg !inline !w-auto p-0 px-4 py-2"
-          // TODO Todo 할일 추가 기능 구현
-          // onClick={async () => {
-          //   create({ title: "새로운 할일", content: "..." });
-          // }}
+    <div className="h-full overflow-auto flex flex-col gap-4">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          selectTodo={selectTodo}
+          isSelected={selectedTodo?.id === todo.id}
         />
-      </h1>
-      <div className="overflow-y-auto flex flex-col gap-4 py-10">
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            selectTodo={selectTodo}
-            isSelected={selectedTodo?.id === todo.id}
-          />
-        ))}
-      </div>
+      ))}
     </div>
   );
 }
-
-const TodoItem = React.memo(function TodoItem({
-  isSelected,
-  selectTodo,
-  todo,
-}: {
-  todo: Todo;
-  selectTodo: (todo?: Todo) => void;
-  isSelected: boolean;
-}) {
-  // TODO TodoItem 기능 구현 해야함
-
-  return (
-    <button
-      // TODO TodoItem 클릭시 선택 기능 구현
-      onClick={() => selectTodo(todo)}
-      className={`w-full flex flex-col gap-4 justify-between p-4 border rounded-lg ${
-        isSelected && "bg-blue-600 text-white"
-      }`}
-    >
-      <h1 className="text-xl font-bold">{todo.title}</h1>
-      <p>{todo.content}</p>
-    </button>
-  );
-});
--- src/pages/todo/ui/TodoPage.tsx
+++ src/pages/todo/ui/TodoPage.tsx
@@ -1,11 +1,13 @@
 import { useCallback, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoader } from "../../../shared/libs";
+import { MainButton } from "../../../shared/ui";
+import todoPageLoader from "../loader/todoPageLoader";
 import { Todo } from "../model/Todo";
 import TodoDetail from "./TodoDetail";
 import { TodoList } from "./TodoList";
 
 export default function TodoPage() {
-  const { todos } = useLoaderData() as { todos: Todo[] };
+  const todos = useLoader<typeof todoPageLoader>();
   const [selectedTodo, setSelectedTodo] = useState<Todo | undefined>(undefined);
 
   const selectTodo = useCallback((todo: Todo | undefined) => {
@@ -13,12 +15,25 @@ export default function TodoPage() {
   }, []);
 
   return (
-    <div className="w-full h-full flex gap-8 items-center max-w-[800px] mx-auto">
-      <TodoList
-        todos={todos}
-        selectTodo={selectTodo}
-        selectedTodo={selectedTodo}
-      />
+    <div className="w-full h-full flex items-center gap-8 max-w-[800px] mx-auto">
+      <div className="w-full h-full flex flex-col gap-4 py-10">
+        <h1 className="text-2xl font-bold flex text-nowrap items-center">
+          할 일 목록
+          <MainButton
+            text="추가"
+            className="ml-auto text-lg !inline !w-auto p-0 px-4 py-2"
+            // TODO Todo 할일 추가 기능 구현
+            // onClick={async () => {
+            //   create({ title: "새로운 할일", content: "..." });
+            // }}
+          />
+        </h1>
+        <TodoList
+          todos={todos}
+          selectTodo={selectTodo}
+          selectedTodo={selectedTodo}
+        />
+      </div>
       <TodoDetail todo={selectedTodo} />
     </div>
   );
