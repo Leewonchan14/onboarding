c39ee4c5fb58c1650ef898b241862227874bb633
이원찬 <twoone14@tukorea.ac.kr>
2024-11-10 23:25:17 +0900

    browser warning 안뜨게 수정

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,26 +1,36 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import { AuthPage } from "../pages/auth";
 import { TodoPage, todoPageLoader } from "../pages/todo";
 import { queryClient } from "../shared/apis";
 import { MainLayout } from "../shared/layouts";
 import { authLoader } from "../shared/loaders";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<MainLayout />}>
+        <Route
+          path=""
+          element={<TodoPage />}
+          loader={authLoader(todoPageLoader(queryClient))}
+        />
+      </Route>
+      <Route path="/auth" element={<AuthPage />} />
+    </>
+  ),
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <TodoPage />,
-        loader: authLoader(todoPageLoader(queryClient)),
-      },
-    ],
-  },
-  {
-    path: "/auth",
-    element: <AuthPage />,
-  },
-]);
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
 
 export default router;
