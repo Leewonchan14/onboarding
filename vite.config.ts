f2f3435db37f91fc85fd0c03ec045a2759a8dfaa
이원찬 <twoone14@tukorea.ac.kr>
2024-11-10 19:37:52 +0900

    vite typechecker 플러그인 설치 및 적용 + vite eslint 플러그인 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslint(),
+    checker({ typescript: { tsconfigPath: "tsconfig.app.json" } }),
+  ],
+});
